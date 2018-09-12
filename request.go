603ec288572b1bff8438c46a46a7f8578444503a
lyric <tiannianshou@gmail.com>
2018-09-12 09:51:56 +0800

    add const

--- request.go
+++ request.go
@@ -15,17 +15,17 @@ var _ Requester = &request{}
 
 // Requester HTTP request interface
 type Requester interface {
-	Head(ctx context.Context, urlStr string, queryParam url.Values, opt ...RequestOption) (Responser, error)
-	Get(ctx context.Context, urlStr string, queryParam url.Values, opt ...RequestOption) (Responser, error)
-	Delete(ctx context.Context, urlStr string, queryParam url.Values, opt ...RequestOption) (Responser, error)
-	Patch(ctx context.Context, urlStr string, queryParam url.Values, opt ...RequestOption) (Responser, error)
-	Post(ctx context.Context, urlStr string, body io.Reader, opt ...RequestOption) (Responser, error)
-	PostJSON(ctx context.Context, urlStr string, body interface{}, opt ...RequestOption) (Responser, error)
-	PostForm(ctx context.Context, urlStr string, body url.Values, opt ...RequestOption) (Responser, error)
-	Put(ctx context.Context, urlStr string, body io.Reader, opt ...RequestOption) (Responser, error)
-	PutJSON(ctx context.Context, urlStr string, body interface{}, opt ...RequestOption) (Responser, error)
-	PutForm(ctx context.Context, urlStr string, body url.Values, opt ...RequestOption) (Responser, error)
-	Do(ctx context.Context, urlStr, method string, body io.Reader, opt ...RequestOption) (Responser, error)
+	Head(ctx context.Context, urlStr string, queryParam url.Values, opts ...RequestOption) (Responser, error)
+	Get(ctx context.Context, urlStr string, queryParam url.Values, opts ...RequestOption) (Responser, error)
+	Delete(ctx context.Context, urlStr string, queryParam url.Values, opts ...RequestOption) (Responser, error)
+	Patch(ctx context.Context, urlStr string, queryParam url.Values, opts ...RequestOption) (Responser, error)
+	Post(ctx context.Context, urlStr string, body io.Reader, opts ...RequestOption) (Responser, error)
+	PostJSON(ctx context.Context, urlStr string, body interface{}, opts ...RequestOption) (Responser, error)
+	PostForm(ctx context.Context, urlStr string, body url.Values, opts ...RequestOption) (Responser, error)
+	Put(ctx context.Context, urlStr string, body io.Reader, opts ...RequestOption) (Responser, error)
+	PutJSON(ctx context.Context, urlStr string, body interface{}, opts ...RequestOption) (Responser, error)
+	PutForm(ctx context.Context, urlStr string, body url.Values, opts ...RequestOption) (Responser, error)
+	Do(ctx context.Context, urlStr, method string, body io.Reader, opts ...RequestOption) (Responser, error)
 }
 
 // RequestURL get request url
@@ -83,15 +83,6 @@ func (r *request) parseQueryParam(urlStr string, param url.Values) string {
 	return urlStr
 }
 
-func (r *request) setContentType(contentType string, opt ...RequestOption) []RequestOption {
-	var ro []RequestOption
-	ro = append(ro, SetHeader("Content-Type", contentType))
-	if len(opt) > 0 {
-		ro = append(ro, opt...)
-	}
-	return ro
-}
-
 func (r *request) fillRequest(req *http.Request, opts ...RequestOption) (*http.Request, error) {
 	if req.Header == nil {
 		req.Header = make(http.Header)
@@ -115,24 +106,33 @@ func (r *request) fillRequest(req *http.Request, opts ...RequestOption) (*http.R
 	return req, nil
 }
 
-func (r *request) doForm(ctx context.Context, urlStr, method string, body url.Values, opt ...RequestOption) (Responser, error) {
+func (r *request) doForm(ctx context.Context, urlStr, method string, body url.Values, opts ...RequestOption) (Responser, error) {
 	var s string
 	if body != nil {
 		s = body.Encode()
 	}
 
-	ro := r.setContentType("application/x-www-form-urlencoded", opt...)
+	var ro []RequestOption
+	ro = append(ro, SetContentType(MIMEApplicationForm))
+	if len(opts) > 0 {
+		ro = append(ro, opts...)
+	}
+
 	return r.Do(ctx, urlStr, method, strings.NewReader(s), ro...)
 }
 
-func (r *request) doJSON(ctx context.Context, urlStr, method string, body interface{}, opt ...RequestOption) (Responser, error) {
+func (r *request) doJSON(ctx context.Context, urlStr, method string, body interface{}, opts ...RequestOption) (Responser, error) {
 	buf := new(bytes.Buffer)
 	err := json.NewEncoder(buf).Encode(body)
 	if err != nil {
 		return nil, err
 	}
 
-	ro := r.setContentType("application/json; charset=UTF-8", opt...)
+	var ro []RequestOption
+	ro = append(ro, SetContentType(MIMEApplicationJSONCharsetUTF8))
+	if len(opts) > 0 {
+		ro = append(ro, opts...)
+	}
 	return r.Do(ctx, urlStr, method, buf, ro...)
 }
 
@@ -149,47 +149,47 @@ func (r *request) httpDo(ctx context.Context, req *http.Request, f func(*http.Re
 	}
 }
 
-func (r *request) Head(ctx context.Context, urlStr string, queryParam url.Values, opt ...RequestOption) (Responser, error) {
-	return r.Do(ctx, r.parseQueryParam(urlStr, queryParam), http.MethodHead, nil, opt...)
+func (r *request) Head(ctx context.Context, urlStr string, queryParam url.Values, opts ...RequestOption) (Responser, error) {
+	return r.Do(ctx, r.parseQueryParam(urlStr, queryParam), http.MethodHead, nil, opts...)
 }
 
-func (r *request) Get(ctx context.Context, urlStr string, queryParam url.Values, opt ...RequestOption) (Responser, error) {
-	return r.Do(ctx, r.parseQueryParam(urlStr, queryParam), http.MethodGet, nil, opt...)
+func (r *request) Get(ctx context.Context, urlStr string, queryParam url.Values, opts ...RequestOption) (Responser, error) {
+	return r.Do(ctx, r.parseQueryParam(urlStr, queryParam), http.MethodGet, nil, opts...)
 }
 
-func (r *request) Delete(ctx context.Context, urlStr string, queryParam url.Values, opt ...RequestOption) (Responser, error) {
-	return r.Do(ctx, r.parseQueryParam(urlStr, queryParam), http.MethodDelete, nil, opt...)
+func (r *request) Delete(ctx context.Context, urlStr string, queryParam url.Values, opts ...RequestOption) (Responser, error) {
+	return r.Do(ctx, r.parseQueryParam(urlStr, queryParam), http.MethodDelete, nil, opts...)
 }
 
-func (r *request) Patch(ctx context.Context, urlStr string, queryParam url.Values, opt ...RequestOption) (Responser, error) {
-	return r.Do(ctx, r.parseQueryParam(urlStr, queryParam), http.MethodPatch, nil, opt...)
+func (r *request) Patch(ctx context.Context, urlStr string, queryParam url.Values, opts ...RequestOption) (Responser, error) {
+	return r.Do(ctx, r.parseQueryParam(urlStr, queryParam), http.MethodPatch, nil, opts...)
 }
 
-func (r *request) Post(ctx context.Context, urlStr string, body io.Reader, opt ...RequestOption) (Responser, error) {
-	return r.Do(ctx, urlStr, http.MethodPost, body, opt...)
+func (r *request) Post(ctx context.Context, urlStr string, body io.Reader, opts ...RequestOption) (Responser, error) {
+	return r.Do(ctx, urlStr, http.MethodPost, body, opts...)
 }
 
-func (r *request) PostJSON(ctx context.Context, urlStr string, body interface{}, opt ...RequestOption) (Responser, error) {
-	return r.doJSON(ctx, urlStr, http.MethodPost, body, opt...)
+func (r *request) PostJSON(ctx context.Context, urlStr string, body interface{}, opts ...RequestOption) (Responser, error) {
+	return r.doJSON(ctx, urlStr, http.MethodPost, body, opts...)
 }
 
-func (r *request) PostForm(ctx context.Context, urlStr string, body url.Values, opt ...RequestOption) (Responser, error) {
-	return r.doForm(ctx, urlStr, http.MethodPost, body, opt...)
+func (r *request) PostForm(ctx context.Context, urlStr string, body url.Values, opts ...RequestOption) (Responser, error) {
+	return r.doForm(ctx, urlStr, http.MethodPost, body, opts...)
 }
 
-func (r *request) Put(ctx context.Context, urlStr string, body io.Reader, opt ...RequestOption) (Responser, error) {
-	return r.Do(ctx, urlStr, http.MethodPut, body, opt...)
+func (r *request) Put(ctx context.Context, urlStr string, body io.Reader, opts ...RequestOption) (Responser, error) {
+	return r.Do(ctx, urlStr, http.MethodPut, body, opts...)
 }
 
-func (r *request) PutJSON(ctx context.Context, urlStr string, body interface{}, opt ...RequestOption) (Responser, error) {
-	return r.doJSON(ctx, urlStr, http.MethodPut, body, opt...)
+func (r *request) PutJSON(ctx context.Context, urlStr string, body interface{}, opts ...RequestOption) (Responser, error) {
+	return r.doJSON(ctx, urlStr, http.MethodPut, body, opts...)
 }
 
-func (r *request) PutForm(ctx context.Context, urlStr string, body url.Values, opt ...RequestOption) (Responser, error) {
-	return r.doForm(ctx, urlStr, http.MethodPut, body, opt...)
+func (r *request) PutForm(ctx context.Context, urlStr string, body url.Values, opts ...RequestOption) (Responser, error) {
+	return r.doForm(ctx, urlStr, http.MethodPut, body, opts...)
 }
 
-func (r *request) Do(ctx context.Context, urlStr, method string, body io.Reader, opt ...RequestOption) (Responser, error) {
+func (r *request) Do(ctx context.Context, urlStr, method string, body io.Reader, opts ...RequestOption) (Responser, error) {
 	if ctx == nil {
 		ctx = context.Background()
 	}
@@ -200,7 +200,7 @@ func (r *request) Do(ctx context.Context, urlStr, method string, body io.Reader,
 		return nil, err
 	}
 
-	req, err = r.fillRequest(req, opt...)
+	req, err = r.fillRequest(req, opts...)
 	if err != nil {
 		return nil, err
 	}
